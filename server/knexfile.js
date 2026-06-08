0d56656546a93330ca9e294cbdb9bafe2a218c11
andcake355 <ninasoldatenko0035@gmail.com>
2026-06-09 01:46:06 +0500

    Update knexfile for production

--- server/knexfile.js
+++ server/knexfile.js
@@ -14,5 +14,18 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, 'db', 'seeds')
     }
+  },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: process.env.SQLITE_FILE || path.join(__dirname, '..', 'database', 'sqlite', 'prod.db')
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   }
 };
